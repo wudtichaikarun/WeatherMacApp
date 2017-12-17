11942bd955229cccf83dddb4bd87500ac669f00f
Romantic <595159090004@dpu.ac.th>
2017-12-17 23:59:30 +0700

    Initial Commit

---
+++ Wether/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wether
+//
+//  Created by Romantic on 17/12/2560 BE.
+//  Copyright © 2560 Romantic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
